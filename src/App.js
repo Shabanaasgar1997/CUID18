85ded3a8bc159676878f6216743036c48b9a8233
Shabanaasgar1997 <shabana.webdeveloper@gmail.com>
2024-11-21 13:37:05 +0530

    updatedone

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ function App() {
   return (
     <div className="App">
       <h1>welcome</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam quisquam
+        libero ipsum, voluptatem harum aliquam officia corporis et? Corporis
+        fuga reiciendis hic perferendis ad quas, velit consectetur modi beatae
+        numquam sit iusto saepe delectus dolor. Odit non fugiat sit,
+        exercitationem aperiam vero accusamus laudantium, aut quisquam corrupti
+        obcaecati maxime cumque?
+      </p>
     </div>
   );
 }
